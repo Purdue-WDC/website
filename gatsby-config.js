5629669268c01adc68bff944ada7138d59abd5d8
Victoire <victoire.beaufil@gmail.com>
2020-12-05 15:06:50 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress-experimental",
+      options: {
+        url: "",
+      },
+    },
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
